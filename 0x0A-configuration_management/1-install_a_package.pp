850929923a5ce762c6fa0c2e241ca0fa8cc7b38a
yusifhuseini <yusifhuseini@gmail.com>
2024-01-19 10:47:09 -0800

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Install flask
+# 1-install_a_package.pp
 
-package { 'flask':
+# Define package resource for Flask
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Notify for verification
+notify { 'Flask installed':
+  message => 'Flask 2.1.0 has been installed successfully.',
 }
