f43c6af9b8cdf1cb28453525a36f510826f92499
yusifhuseini <yusifhuseini@gmail.com>
2024-01-19 12:09:15 -0800

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
-#!/usr/bin/pup
+# Installs puppet-lint
 
-# Install flask
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => gem,
 }
