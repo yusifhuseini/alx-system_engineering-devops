20e13f9f0c6d1767713b3f6340d4c1beeffc62f2
yusifhuseini <yusifhuseini@gmail.com>
2024-01-19 12:10:15 -0800

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs puppet-lint
+# install puppet-lint -v 2.5.0
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => gem,
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
