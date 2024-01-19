8db0ca8d93c568c76719b94491e20491615b70db
yusifhuseini <yusifhuseini@gmail.com>
2024-01-19 15:41:36 -0800

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Installs flask from pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#Install flask version 2.1.0
+
+
+exec {'pip3 install flask':
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
+}
+
+exec {'python-installed':
+command => '/usr/bin/which python3'
 }
