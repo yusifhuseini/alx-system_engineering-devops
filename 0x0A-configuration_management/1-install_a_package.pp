de7e4d11cfddb9f3da76f31679517ceeb5e40ee5
yusifhuseini <yusifhuseini@gmail.com>
2024-01-19 10:57:55 -0800

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
-# 1-install_a_package.pp
+# Puppet manifest for installing Flask
 
-# Define package resource for Flask
 package { 'Flask':
-  ensure   => '2.1.0',
+  ensure => '2.1.0',
   provider => 'pip3',
-}
-
-# Notify for verification
-notify { 'Flask installed':
-  message => 'Flask 2.1.0 has been installed successfully.',
 }
