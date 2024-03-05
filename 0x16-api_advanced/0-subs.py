b4063b8b9648270b3a4ae1964c19b0999a6ea37d
yusifhuseini <yusifhuseini@gmail.com>
2024-03-05 22:33:43 +0000

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Script that queries the Reddit API and returns the number of subscribers
+for a given subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """Returns the total number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "My-User-Agent"}
+
+    response = requests.get(url,
+                            headers=headers,
+                            allow_redirects=False)
+    if response.status_code >= 300:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    else:
+        data = response.json().get("data")
+        subscribers = data.get("subscribers")
+        return subscribers
