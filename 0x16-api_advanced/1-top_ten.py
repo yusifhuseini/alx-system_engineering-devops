e5ed813d74af48c5767bea3688fa5a3497b75c99
yusifhuseini <yusifhuseini@gmail.com>
2024-03-06 05:20:59 +0000

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,6 +3,7 @@
 Defines a function that queries Reddit API
 """
 import requests
+import sys
 
 
 def top_ten(subreddit):
@@ -12,12 +13,22 @@ def top_ten(subreddit):
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'advanced-api/0.0.1 by Huseini'}
-    req = requests.get(url=url, headers=headers, allow_redirects=False)
-    if req.status_code == 200:
+    try:
+        req = requests.get(url=url, headers=headers, allow_redirects=False)
+        req.raise_for_status()  # Raises HTTPError for bad status codes
         response = req.json()
         titles = [child['data']['title']
                   for child in response['data']['children'][:10]]
         for title in titles:
             print(title)
-    else:
-        print(None)
+    except requests.HTTPError as e:
+        print("Error:", e)
+    except Exception as e:
+        print("An unexpected error occurred:", e)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <subreddit>")
+        sys.exit(1)
+    top_ten(sys.argv[1])
